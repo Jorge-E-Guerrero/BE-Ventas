5e6ca8199ddc5bb5ad0f2b1849d28c12c57c4df3
Jorge-E-Guerrero <guerrero191096@unis.edu.gt>
2022-09-22 19:14:32 -0600

    Update InventarioControlador.java

--- src/main/java/net/codejava/Controlador/InventarioControlador.java
+++ src/main/java/net/codejava/Controlador/InventarioControlador.java
@@ -159,6 +159,24 @@ public class InventarioControlador {
 		if(true){
 			_n = n.get();
 		}
+		if(true){
+			_n = n.get();
+		}
+		if(true){
+			_n = n.get();
+		}
+		if(true){
+			_n = n.get();
+		}
+		if(true){
+			_n = n.get();
+		}
+		if(true){
+			_n = n.get();
+		}
+		if(true){
+			_n = n.get();
+		}
 		
 
 		if(nCategoriaDipositivo != "") {
